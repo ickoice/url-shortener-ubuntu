8e7a937a9dfdc14313721501ff16b90514fbf334
Risto Radanliev <ickoice@hotmail.com>
2020-10-25 17:28:51 +0100

    initial

---
+++ index.js
@@ -0,0 +1,121 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const yup = require("yup");
+const { nanoid } = require('nanoid');
+const mongoose = require('mongoose');
+
+var app = express();
+
+//Set up default mongoose connection
+var mongoDB = 'mongodb://127.0.0.1/url-shortener';
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+//Bind connection to error event (to get notification of connection errors)
+db.once("open", function() {
+    console.log("Connection Successful!");
+});
+
+require('dotenv').config();
+
+app.use(express.json());
+app.use(express.static('./public'));
+
+
+app.use(helmet());
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
+
+
+var schema = mongoose.Schema({
+    url: String,
+    slug: String
+});
+  
+var Model = mongoose.model("model", schema, "myCollection");
+
+app.get('/:id', (req, res) => {
+
+    const {id: slug} = req.params;
+    
+    try {
+       
+        Model.find({ 'slug': slug })
+            .then(url=> {
+                console.log(url);
+
+                if (url[0]) {
+                    console.log("here");
+                    console.log(url[0].url);
+                    
+                    res.redirect(url[0].url).end();
+                }
+                console.log("now here");
+                res.redirect(`/?error=${slug} not found what the fuck`).end();
+            });
+            // .catch(
+            //     res.redirect(`/?error=${slug} not found`).end()
+            // ); 
+
+    } catch (error) {
+
+        console.log(here);
+        res.redirect(`/?error=Link not found`);
+    }
+
+});
+
+app.post('/url', async (req, res, next) => {
+
+    let { slug, url } = req.body;
+    
+    try {
+    
+        if (!slug) {
+            slug = nanoid(5);
+        } 
+        slug = slug.toLowerCase();
+        
+        var url1 = new Model({ slug: slug, url: url });
+
+        await url1.save(function(err, urlResponse) {
+          if (err) return console.error(err);
+          console.log("Document inserted succussfully!");
+
+          const created = urlResponse;
+          res.json(created);
+        });
+        
+    } catch (error) {
+        next(error);
+    }
+    
+});
+
+app.use((error, req, res, next) => {
+    console.log("eroorrrrr");
+    if (error.status){
+        res.status(error.status);
+    } else {
+        res.status(500);
+    }
+
+    res.json({
+        message: error.message,
+        stack: process.env.NODE_ENV === 'production' ? 'cake' : error.stack,
+    });
+});
+
+const port = process.env.PORT || 1337;
+
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`);
+});
---
+++ public/app.js
@@ -0,0 +1,61 @@
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        url: '',
+        slug: '',
+        created: null
+    },
+    methods: {
+        async createUrl() {
+            const response = await fetch('/url', {
+                method: 'POST',
+                headers: {
+                    'Content-Type' : 'application/json',
+                },
+                body: JSON.stringify({
+                    url: this.url,
+                    slug: this.slug
+                })
+            });
+            this.created = await response.json();
+        }
+    }
+});
+
+// var button = document.getElementById("submitButton");
+// button.addEventListener("click", function(event) {
+//     event.preventDefault();
+//     createUrl();
+// });
+
+// async function createUrl() {
+
+//     var url = document.getElementById("url").value;
+//     var slug = document.getElementById("slug").value;
+
+//     // const response = await fetch('/url', {
+//     //     method: 'POST',
+//     //     headers: {
+//     //         'Content-Type' : 'application/json',
+//     //     },
+//     //     body: JSON.stringify({
+//     //         url: url,
+//     //         slug: slug
+//     //     })
+//     // });
+//     // var created = await response.json();
+
+//     var xhr = new XMLHttpRequest();
+    
+//     xhr.open("POST", window.location.href + "url", true);
+//     xhr.setRequestHeader("Content-Type", "application/json");
+//     xhr.onreadystatechange = function () {
+//         if (xhr.readyState === 4 && xhr.status === 200) {
+//             var json = JSON.parse(xhr.responseText);
+//             console.log(json.slug + ", " + json.url);
+//         }
+//     };
+//     var data = JSON.stringify({"url": url, "slug": slug});
+//     xhr.send(data);
+// }
